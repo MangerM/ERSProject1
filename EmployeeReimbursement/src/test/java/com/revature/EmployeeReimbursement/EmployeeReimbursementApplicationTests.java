e0ebd31bc0a3f0bd679f063358c0224c47e6c466
MangerM <mangerml1891@gmail.com>
2024-04-23 16:20:39 -0400

    Initial commit

---
+++ EmployeeReimbursement/src/test/java/com/revature/EmployeeReimbursement/EmployeeReimbursementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.EmployeeReimbursement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeReimbursementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ERSProject1
 EmployeeReimbursementProject 1
+
+
+This project will design and create an employee reimbursement system front and back end.
